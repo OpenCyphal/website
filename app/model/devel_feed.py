0931327af58c70f27914189ace094e5e00fcb000
Pavel Kirienko <pavel.kirienko@gmail.com>
2019-02-01 15:35:40 +0200

    Devel feed fixes

--- app/model/devel_feed.py
+++ app/model/devel_feed.py
@@ -10,19 +10,21 @@ from . import cache
 
 
 class Entry:
-    def __init__(self, text, timestamp):
+    def __init__(self, text, timestamp, is_important):
         self.text = str(text)
         self.timestamp = timestamp
+        self.is_important = bool(is_important)
 
     @staticmethod
     def new(d: dict):
+        is_important = False
+
         if d['type'] == 'WatchEvent':
             text = ' '.join([
                 _render_url(d['actor']['login'], d['actor']['url']),
                 'starred',
                 _render_url(d['repo']['name'], d['repo']['url']),
             ])
-            timestamp = _strptime(d['created_at'])
 
         elif d['type'] == 'PullRequestEvent' and d['payload']['action'] in ('opened', 'closed', 'reopened'):
             text = ' '.join([
@@ -35,7 +37,6 @@ class Entry:
                 'at',
                 _render_url(d['repo']['name'], d['repo']['url']),
             ])
-            timestamp = _strptime(d['payload']['pull_request']['created_at'])
 
         elif d['type'] == 'IssuesEvent' and d['payload']['action'] in ('opened', 'closed', 'reopened'):
             text = ' '.join([
@@ -48,7 +49,6 @@ class Entry:
                 'at',
                 _render_url(d['repo']['name'], d['repo']['url']),
             ])
-            timestamp = _strptime(d['payload']['issue']['created_at'])
 
         elif d['type'] == 'ForkEvent':
             text = ' '.join([
@@ -56,34 +56,48 @@ class Entry:
                 'forked',
                 _render_url(d['repo']['name'], d['repo']['url']),
             ])
-            timestamp = _strptime(d['payload']['forkee']['created_at'])
 
-        elif d['type'] == 'PushEvent':
+        elif d['type'] == 'CreateEvent':
+            actor = _render_url(d['actor']['login'], d['actor']['url'])
+            repo = _render_url(d['repo']['name'], d['repo']['url'])
+            if d['payload']['ref_type'] in ('branch', 'tag'):
+                text = ' '.join([
+                    actor,
+                    'created a new',
+                    d['payload']['ref_type'],
+                    '&ldquo;' + d['payload']['ref'] + '&rdquo;',
+                    'at',
+                    repo,
+                ])
+            elif d['payload']['ref_type'] in ('repository',):
+                is_important = True
+                text = ' '.join([
+                    actor,
+                    'created a new repository',
+                    repo,
+                ])
+            else:
+                raise ValueError('Unexpected create event type: %r' % d['payload']['ref_type'])
+
+        elif d['type'] == 'ReleaseEvent' and d['payload']['action'] == 'published':
+            is_important = True
             text = ' '.join([
                 _render_url(d['actor']['login'], d['actor']['url']),
-                'pushed',
-                str(len(d['payload']['commits'])),
-                'commits to',
-                _render_url(d['repo']['name'], d['repo']['url']),
-            ])
-            timestamp = _strptime(d['created_at'])
-
-        elif d['type'] == 'IssueCommentEvent' and d['payload']['action'] in ('created', ):
-            text = ' '.join([
-                _render_url(d['actor']['login'], d['actor']['url']),
-                'commented on',
+                'released',
                 '&ldquo;' +
-                _render_url(d['payload']['issue']['title'], d['payload']['issue']['html_url']) +
+                _render_url(d['payload']['release']['name'].strip() or d['payload']['release']['tag_name'],
+                            d['payload']['release']['html_url']) +
                 '&rdquo;',
                 'at',
                 _render_url(d['repo']['name'], d['repo']['url']),
             ])
-            timestamp = _strptime(d['payload']['issue']['created_at'])
 
         else:
             return
 
-        return Entry(text=text, timestamp=timestamp)
+        return Entry(text=text,
+                     timestamp=_strptime(d['created_at']),
+                     is_important=is_important)
 
 
 def get():
@@ -96,7 +110,8 @@ def get():
             # noinspection PyBroadException
             try:
                 e = Entry.new(event)
-                if e:
+                # Do not add similar entries one after another
+                if e and (len(entries) == 0 or e.text != entries[-1].text):
                     entries.append(e)
             except Exception:
                 app.logger.exception('Could not process event entry')
