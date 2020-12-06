b78b5b71cc3c5570f1538cacc06c2b489cb2baf7
Clyde Johnston <clyde@k-space.ee>
2020-12-06 03:35:59 +0200

    Added 'UAVCAN Guide' page in HTML format

--- app/view/guide.py
+++ app/view/guide.py
@@ -3,13 +3,24 @@
 # Author: Pavel Kirienko <pavel.kirienko@zubax.com>
 #
 
+import os
+from flask import render_template, send_file, abort
 from .. import app
-from flask import redirect
+from .home import TITLE
 
 
-_GUIDE_URL = 'https://forum.uavcan.org/t/778'
+_GUIDE_DIRECTORY_PATH = os.path.join(app.root_path, '..', 'guide')
 
 
-@app.route('/guide/')
-def _guide():
-    return redirect(_GUIDE_URL)
+@app.route('/guide')
+def guide():
+    return render_template('guide.html',
+                           title=TITLE)
+
+
+@app.route('/guide/<path:file_name>')
+def _guide_document(file_name):
+    try:
+        return send_file(os.path.join(_GUIDE_DIRECTORY_PATH, file_name))
+    except FileNotFoundError:
+        return abort(404)
