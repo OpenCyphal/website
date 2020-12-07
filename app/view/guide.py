67e0c0c5fc2e2785bcfacd65233d716e285158ec
Clyde Johnston <clyde@k-space.ee>
2020-12-08 01:08:17 +0200

    Changed 'Guide' navigation to point to UAVCAN forum post

--- app/view/guide.py
+++ app/view/guide.py
@@ -3,24 +3,13 @@
 # Author: Pavel Kirienko <pavel.kirienko@zubax.com>
 #
 
-import os
-from flask import render_template, send_file, abort
 from .. import app
-from .home import TITLE
+from flask import redirect
 
 
-_GUIDE_DIRECTORY_PATH = os.path.join(app.root_path, '..', 'guide')
+_GUIDE_URL = 'https://forum.uavcan.org/t/778'
 
 
-@app.route('/guide')
-def guide():
-    return render_template('guide.html',
-                           title=TITLE)
-
-
-@app.route('/guide/<path:file_name>')
-def _guide_document(file_name):
-    try:
-        return send_file(os.path.join(_GUIDE_DIRECTORY_PATH, file_name))
-    except FileNotFoundError:
-        return abort(404)
+@app.route('/guide/')
+def _guide():
+    return redirect(_GUIDE_URL)
